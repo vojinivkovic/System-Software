be93701fda9d12c0ec6c4491c1546bf3a47ef446
vojinii <vojinivkovic02@gmail.com>
2026-05-10 05:36:47 -0700

    Add the majority of instruction exceptions

--- aux/instructions_functions.cpp
+++ aux/instructions_functions.cpp
@@ -1,6 +1,7 @@
 #include "instructions_functions.hpp"
 #include "../assembler.hpp"
 #include "instructions.hpp"
+#include "exceptions.hpp"
 
 enum class UnconditionalJumpType 
 {
@@ -57,13 +58,126 @@ static uint32_t resolveLiteral(const std::string& literal)
     return operand;
 }
 
+
+static void exceptionInstructionConditionalJump(const std::vector<Argument>& arguments)
+{
+
+  if(arguments.size() > 3) 
+  {
+    throw AssemblerErrors(ErrorType::ErrorTooManyArguments, "Instruction [.beq/bne/bgt] except three arguments", 
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+  if(arguments.size() == 0) 
+  {
+    throw AssemblerErrors(ErrorType::ErrorTooFewArguments, "Instruction [.beq/bne/bgt] except three argument",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+  if(arguments[0].type != ArgumentType::Register || arguments[1].type != ArgumentType::Register || (arguments[2].type != ArgumentType::OperandLiteral && arguments[2].type != ArgumentType::OperandSymbol))
+  {
+        throw AssemblerErrors(ErrorType::ErrorInvalidArgument, "Instruction [.beq/bne/bgt] must have registers and symbol/literal as operand",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+}
+
+static void exceptionInstructionsALU(const std::vector<Argument>& arguments)
+{
+
+  if(arguments.size() > 2) 
+  {
+    throw AssemblerErrors(ErrorType::ErrorTooManyArguments, "ALU instructions except two arguments", 
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+  if(arguments.size() == 0) 
+  {
+    throw AssemblerErrors(ErrorType::ErrorTooFewArguments, "ALU instructions except two argument",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+  
+  if(arguments[0].type != ArgumentType::Register || arguments[1].type != ArgumentType::Register)
+  {
+        throw AssemblerErrors(ErrorType::ErrorInvalidArgument, "ALU instructions must have registers as operands",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+}
+
+static void exceptionInstructions_Not_Push_Pop(const std::vector<Argument>& arguments)
+{
+
+  if(arguments.size() > 1) 
+  {
+    throw AssemblerErrors(ErrorType::ErrorTooManyArguments, "Instructions [.not/push/pop] except only one argument", 
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+  if(arguments.size() == 0) 
+  {
+    throw AssemblerErrors(ErrorType::ErrorTooFewArguments, "Instructions [.not/push/pop] except only one argument",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+  
+  if(arguments[0].type != ArgumentType::Register)
+  {
+        throw AssemblerErrors(ErrorType::ErrorInvalidArgument, "Instructions [.not/push/pop] except only one argument as operand",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+}
+
+static void exceptionSpecialRegisters(const std::vector<Argument>& arguments, bool read)
+{
+  if(arguments.size() > 2) 
+  {
+    throw AssemblerErrors(ErrorType::ErrorTooManyArguments, "Instructions [.csrrd/csrwr] except two arguments", 
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+
+  if(arguments.size() == 0) 
+  {
+    throw AssemblerErrors(ErrorType::ErrorTooFewArguments, "Instructions [.csrrd/csrwr] except two arguments",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+
+  if(arguments[0].type != ArgumentType::Register || arguments[1].type != ArgumentType::Register)
+  {
+        throw AssemblerErrors(ErrorType::ErrorInvalidArgument, "Instructions [.csrrd/csrwr] must have registers as operands",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+
+  if(read)
+  {
+    if(arguments[0].registerNum > 3)
+    {
+              throw AssemblerErrors(ErrorType::ErrorInvalidArgument, "Invalid special purpose register as operand",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());  
+    }
+
+  }
+  else
+  {
+    if(arguments[1].registerNum > 3)
+    {
+              throw AssemblerErrors(ErrorType::ErrorInvalidArgument, "Invalid special purpose register as operand",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());  
+    }
+  }
+
+}
 std::vector<uint8_t> instructionHalt(const std::vector<Argument> &arguments)
 {
+  if(arguments.size() > 0)
+  {
+        throw AssemblerErrors(ErrorType::ErrorTooManyArguments, "Instruction [.halt] doesn't have arguments", 
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
   return std::vector<uint8_t>{0x00, 0x00, 0x00, 0x00};
 }
 
 std::vector<uint8_t> instructrionSoftwareInterrupt(const std::vector<Argument> &arguments)
 {
+    if(arguments.size() > 0)
+  {
+        throw AssemblerErrors(ErrorType::ErrorTooManyArguments, "Instruction [.int] doesn't have arguments", 
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+
+  }
   return std::vector<uint8_t>{0x10, 0x00, 0x00, 0x00};
 }
 
@@ -72,6 +186,23 @@ static std::vector<uint8_t> instructionJumpOrCall(const std::vector<Argument> &a
   std::vector<uint8_t> instr;
   uint32_t operand;
   Argument arg = arguments[0];
+  if(arguments.size() > 1) 
+  {
+    throw AssemblerErrors(ErrorType::ErrorTooManyArguments, "Instructions [.jmp/call] except only one argument", 
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+  if(arguments.size() == 0) 
+  {
+    throw AssemblerErrors(ErrorType::ErrorTooFewArguments, "Instructions [.jmp/call] except only one argument",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+  }
+
+  if(arg.type != ArgumentType::OperandLiteral && arg.type != ArgumentType::OperandSymbol)
+  {
+        throw AssemblerErrors(ErrorType::ErrorInvalidArgument, "Instructions [.jmp/call] except only literal or symbol",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+ 
+  }
   if(typeOfJump == UnconditionalJumpType::Call)
   {
     instr.push_back(0x20);
@@ -92,6 +223,12 @@ static std::vector<uint8_t> instructionJumpOrCall(const std::vector<Argument> &a
   {
     if(Instructions::resolveSymbol(arg.variable, &operand))
     {
+      if(((int)operand) < 0)
+      {
+        throw AssemblerErrors(ErrorType::ErrorInvalidArgument, "Instructions [.jmp/call] can only have positive value for adresses",
+        Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+ 
+      }
       instr.push_back(operand >> 8 & 0x0F);
       instr.push_back(operand & 0xFF);
     }
@@ -118,6 +255,8 @@ static std::vector<uint8_t> instructionConditionalJump(const std::vector<Argumen
 {
   std::vector<uint8_t> instr;
   uint32_t operand;
+
+  exceptionInstructionConditionalJump(arguments);
   switch (typeOfJump)
   {
   case ConditionalJumpType::Equal:
@@ -136,6 +275,12 @@ static std::vector<uint8_t> instructionConditionalJump(const std::vector<Argumen
 
   if(Instructions::resolveSymbol(arguments[2].variable, &operand))
   {
+    if(((int)operand) < 0)
+    {
+      throw AssemblerErrors(ErrorType::ErrorInvalidArgument, "Instructions [.beq/bnt/bgt] can only have positive value for adresses",
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+
+    }
     instr.push_back((arguments[1].registerNum << 4) | (operand >> 8 & 0x0F));
     instr.push_back(operand & 0XFF);
   }
@@ -166,6 +311,7 @@ std::vector<uint8_t> instuctionJumpGreater(const std::vector<Argument> &argument
 
 std::vector<uint8_t> instructionAtomicExchange(const std::vector<Argument> &arguments)
 {
+  exceptionInstructionsALU(arguments);
   std::vector<uint8_t> instr{0x40};
   instr.push_back(arguments[0].registerNum);
   instr.push_back(arguments[1].registerNum << 4);
@@ -176,6 +322,7 @@ std::vector<uint8_t> instructionAtomicExchange(const std::vector<Argument> &argu
 
 static std::vector<uint8_t> instructionArithmeticOperation(const std::vector<Argument>& arguments, const ArithmeticOperationType& typeOfOperation)
 {
+  exceptionInstructionsALU(arguments);
   std::vector<uint8_t> instr;
   switch (typeOfOperation)
   {
@@ -226,6 +373,15 @@ std::vector<uint8_t> instructionDiv(const std::vector<Argument> &arguments)
 
 static std::vector<uint8_t> instructionLogicalOperation(const std::vector<Argument> &arguments, const LogicalOpetaionType& typeOfOperation)
 {
+  if(typeOfOperation != LogicalOpetaionType::Not)
+  {
+    exceptionInstructionsALU(arguments);
+  }
+  else
+  {
+    exceptionInstructions_Not_Push_Pop(arguments);
+  }
+
   std::vector<uint8_t> instr;
   switch (typeOfOperation)
   {
@@ -282,6 +438,7 @@ std::vector<uint8_t> instructionXor(const std::vector<Argument> &arguments)
 
 static std::vector<uint8_t> instructionShiftOperation(const std::vector<Argument> &arguments, const ShiftOperationType& typeOfOperation)
 {
+  exceptionInstructionsALU(arguments);
   std::vector<uint8_t> instr;
   switch(typeOfOperation)
   {
@@ -312,6 +469,7 @@ std::vector<uint8_t> instructionShiftRight(const std::vector<Argument> &argument
 
 std::vector<uint8_t> instructionPush(const std::vector<Argument> &arguments)
 {
+  exceptionInstructions_Not_Push_Pop(arguments);
   std::vector<uint8_t> instr{0x81, 0xE0};
   instr.push_back((arguments[0].registerNum << 4) | 0xF);
   instr.push_back(0xFC);
@@ -320,6 +478,7 @@ std::vector<uint8_t> instructionPush(const std::vector<Argument> &arguments)
 
 std::vector<uint8_t> instructionPop(const std::vector<Argument> &arguments)
 {
+  exceptionInstructions_Not_Push_Pop(arguments);
   std::vector<uint8_t> instr{0x93};
   instr.push_back((arguments[0].registerNum << 4) | 0xE);
   instr.push_back(0x00);
@@ -329,13 +488,19 @@ std::vector<uint8_t> instructionPop(const std::vector<Argument> &arguments)
 
 std::vector<uint8_t> instructionReturnFromInterrupt(const std::vector<Argument> &arguments)
 {
+    if(arguments.size() > 0)
+  {
+        throw AssemblerErrors(ErrorType::ErrorTooManyArguments, "Instruction [.halt] doesn't have arguments", 
+      Assembler::getCurrentSection()->getSectionName(), Assembler::getCurrentSection()->getLocationCounter());
+
+  }
   // POP PC;
   std::vector<uint8_t> instr{0x93};
   instr.push_back((0xF << 4) | 0xE);
   instr.push_back(0x00);
   instr.push_back(0x04);
 
-  //POP STATUS
+  //POP STATUS;
   instr.push_back(0x97);
   instr.push_back(0x0E);
   instr.push_back(0x00);
@@ -346,6 +511,7 @@ std::vector<uint8_t> instructionReturnFromInterrupt(const std::vector<Argument> 
 
 std::vector<uint8_t> instructionReadFromCSRegister(const std::vector<Argument> &arguments)
 {
+  exceptionSpecialRegisters(arguments, true);
   std::vector<uint8_t> instr{0x90};
   instr.push_back((arguments[1].registerNum << 4) | arguments[0].registerNum);
   instr.push_back(0x00);
@@ -355,7 +521,7 @@ std::vector<uint8_t> instructionReadFromCSRegister(const std::vector<Argument> &
 
 std::vector<uint8_t> instructionWriteToCSRegister(const std::vector<Argument> &arguments)
 {
-
+  exceptionSpecialRegisters(arguments, false);
   std::vector<uint8_t> instr{0x94};
   instr.push_back((arguments[1].registerNum << 4) | arguments[0].registerNum);
   instr.push_back(0x00);
